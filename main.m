fac7025f0783fc358b05199f07cef99efc937c1d
Reem Muhammad <reem_mohamed_27@hotmail.com>
2020-02-07 09:19:19 +0200

    Added performance measures

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ addpath('.\Features');
 dataDir = '';
 windowSize = 256;
 FEATURE_MATRIX = []; %feature matrix for all records
-
+LABELS = [];
 %surrogateChannel_Extract();
 %labelling();
 
@@ -32,8 +32,51 @@ for case_iter = cases
             
             featureMatrix_record= featureMatrix_Construct(combinedIMF, windowSize);
             FEATURE_MATRIX = [FEATURE_MATRIX; featureMatrix_record];
+            LABELS = [LABELS; record_labels];
         end
      end
 end
+
+N_trainingWindows = 0.8 * length(FEATURE_MATRIX);
+N_testingWindows = 0.2 * length(FEATURE_MATRIX);
+
+train_idx = randperm(size(FEATURE_MATRIX,1), N_trainingWindows);
+test_idx = ~ismember([1:size(FEATURE_MATRIX,1)], train_idx);
+
+x_train = FEATURE_MATRIX(train_idx, :);  % 80% of the feature matrix
+y_train = LABELS(train_idx);             % 80% of label matrix
+
+x_test = FEATURE_MATRIX(test_idx,:);   % 20% of the feature matrix
+y_test = LABELS(test_idx);             % 20% of label matrix
+
+
+model = svmtrain(x_train, y_train, 'kernel_function', 'rbf');
+results = svmclassify(model, x_test);
+
+%Classification performance
+
+TP = 0;
+TN = 0;
+FP = 0;
+FN = 0;
+
+for idx = [1:length(y_test)]
+    if(y_test(idx) == results(idx) && y_test(idx) == 1)
+        TP = TP + 1;
+    end
+    if(y_test(idx) == results(idx) && y_test(idx) == 0)
+        TN = TN + 1;
+    end
+    if(y_test(idx) == 1 && results(idx) == 0)
+        FN = FN + 1;
+    end
+    if(y_test(idx) == 0 && results(idx) == 01)
+        FP = FP + 1;
+    end
+end
+
+sensitivity = TP/(TP + FN);
+specificity = TN/(FP + TN);
+accuracy = ((TP+TN)/length(y_test))*100;
 
 
